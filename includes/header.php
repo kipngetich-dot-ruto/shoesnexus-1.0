e277c6c513a264ebec2053938ef3962eb98433ab
Kipngetich Ruto <kipngetich.ruto@outlook.com>
2024-11-20 00:12:06 +0300

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Shoes Nexus | The best online shop for all types of shoes.
+    </title>
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="nav_sn p-r p-l">
+    <div class="nav_brand_sn">
+        <a href="/shoesnexus"><span>Shoes</span>Nexus</a>
+    </div>
+    <div class="nav_sn_menu">
+        <form action="">
+            <input 
+                type="text" 
+                name="search"
+                class="nav_search_input"
+                placeholder="Search for a product here"
+            >
+            <button class="btn btn-search">Search</button>
+        </form>
+        <ul>
+            <li>
+                <button class="btn"><span>Icon</span> Account</button>
+            </li>
+            <li>
+                <button class="btn"><span>Icon</span> Help</button>
+            </li>
+            <li>
+                <a href="" class="btn"><span>Icon</span> About</a>
+            </li>
+            <li>
+                <a href=""><span>Icon</span> Cart</a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include_once "includes/header.php";
+?>
+
+ <!-- Main Content -->
+<div class="hero">
+    <div class="container p-5">
+        <h1>Welcome to ShopNexus!</h1>
+        <p>Explore our wide range of products and exclusive deals.</p>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include_once "includes/footer.php";
